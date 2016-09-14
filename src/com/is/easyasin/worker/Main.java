1e5b3491388919c84eefb9cf3eb290901c9479b2
Matt Myers <Matt.Lucian@gmail.com>
2016-09-14 16:24:46 -0400

    Initial Project Commit

---
+++ src/com/is/easyasin/worker/Main.java
@@ -0,0 +1,19 @@
+package com.is.easyasin.worker;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Read CSV file into Product objects
+
+        // Output JSON File with the following rules :
+            // sellPrice must be 140% of the price
+            // Make the title all caps
+
+        // Output XML File with the following rules :
+            // sellPrice must be 175% of the price
+            // Strip out the first 2 characters of the SKU
+            // only "new" products
+
+    }
+}
---
+++ src/com/is/easyasin/worker/Product.java
@@ -0,0 +1,63 @@
+package com.is.easyasin.worker;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Matt on 9/14/2016.
+ */
+
+public class Product {
+
+    private String title;
+    private String upc;
+    private String sku;
+    private BigDecimal price;
+    private BigDecimal sellPrice;
+    private String condition;
+    private Integer quantity;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() { return price; }
+
+    public void setPrice(BigDecimal price) { this.price = price; }
+
+    public BigDecimal getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(BigDecimal sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public String getCondition() { return condition; }
+
+    public void setCondition(String condition) { this.condition = condition; }
+
+
+    public Integer getQuantity() { return quantity; }
+
+    public void setQuantity(Integer quantity) { this.quantity = quantity; }
+
+
+    public String getSku() { return sku; }
+
+    public void setSku(String sku) { this.sku = sku; }
+
+
+    public String getUpc() {
+        return upc;
+    }
+
+    public void setUpc(String upc) {
+        this.upc = upc;
+    }
+
+}
